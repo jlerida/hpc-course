d20e6bb47ea158cf48318c79b35f4ad89d50e427
jlerida <jlerida@diei.udl.cat>
2016-04-25 10:37:20 +0200

    Adding Activity solutions code

--- Activity1/mpi_hello.c
+++ Activity1/mpi_hello.c
@@ -12,11 +12,13 @@ int main (int argc, char* argv[])
 {
     int rank, size;
     char hostname[256];
+    int namelen;
     
     MPI_Init (&argc, &argv);      /* starts MPI */
-    MPI_Comm_rank (MPI_COMM_WORLD, &rank);        /* get current process id */
-    MPI_Comm_size (MPI_COMM_WORLD, &size);        /* get number of processes */
-    gethostname(hostname,255);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);        // get current process id
+    MPI_Comm_size (MPI_COMM_WORLD, &size);        // get number of processes
+    MPI_Get_processor_name(hostname, &namelen);   // get CPU name
+
     printf( "Hello world from process %d of %d (node %s)\n", rank, size, hostname );
     MPI_Finalize();
     return 0;
---
+++ Activity2/mpi_pingpongchar.c
@@ -0,0 +1,36 @@
+#include "mpi.h"
+#include <stdio.h>
+
+main(int argc, char *argv[])  {
+    int numtasks, namelen, rank, dest, source, rc, count, tag=1;
+    char inmsg, outmsg='x';
+    char hostname[256];
+    MPI_Status Stat;
+    
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);   // get number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);       // get current process id
+    MPI_Get_processor_name(hostname, &namelen); // get CPU name
+    
+    if (rank == 0) {
+        dest = 1;
+        source = 1;
+        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+        printf( "process %d of %d (node %s)\n", rank, numtasks, hostname );
+    }
+    
+    else if (rank == 1) {
+        dest = 0;
+        source = 0;
+        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+        printf( "process %d of %d (node %s)\n", rank, numtasks, hostname );
+    }
+    
+    rc = MPI_Get_count(&Stat, MPI_CHAR, &count);
+    printf("Task %d: Received %d char(s) from task %d with tag %d \n",
+           rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
+    
+    MPI_Finalize();
+}
